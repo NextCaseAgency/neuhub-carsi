f9fda25e9692b78b62c829e60d3f69ffcfdf1e87
d3pi3dr4 <denizsinann07@gmail.com>
2025-10-17 09:50:21 +0300

    remove debug statement from sanat-galerisi.blade.php

--- resources/views/sanat-galerisi.blade.php
+++ resources/views/sanat-galerisi.blade.php
@@ -20,9 +20,11 @@
     <!-- header end -->
     <!-- galery start -->
     <section id="galery-banner" class="mb-5">
-      <div class="galery-home" style="background: url({{$data[0]['data']['image']}}) center center no-repeat; background-size: cover;
-    width: 100%;
-    min-height: 400px;">
+        <div class="galery-home"
+             style="background: url('{{ asset($data[0]['data']['image']) }}') center center no-repeat;
+            background-size: cover;
+            width: 100%;
+            min-height: 400px;">
         <div class="container" style="padding: 0 48px;">
             <h2 class="galery-home-title" style="font-weight: 500;">{{ $data[3]['data']['title'] }}</h2>
             <span>{{ $data[4]['data']['title'] }}</span>

d5689af8d88d9766c8c1b5d1c81b7ec4e2049b8f
d3pi3dr4 <denizsinann07@gmail.com>
2025-10-17 09:45:25 +0300

    project updated

--- resources/views/sanat-galerisi.blade.php
+++ resources/views/sanat-galerisi.blade.php
@@ -19,9 +19,10 @@
     </header>
     <!-- header end -->
 
+    @dd($data)
     <!-- galery start -->
     <section id="galery-banner" class="mb-5">
-      <div class="galery-home">
+      <div class="galery-home" style="background: url(../img/banner.png);">
         <div class="container" style="padding: 0 48px;">
             <h2 class="galery-home-title" style="font-weight: 500;">{{ $data[3]['data']['title'] }}</h2>
             <span>{{ $data[4]['data']['title'] }}</span>
